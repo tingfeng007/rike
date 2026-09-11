0a9c6f24737b639322e288382cec1caa8c7df06b
tingfeng007 <ganggexiang@gmail.com>
2026-09-11 19:04:41 +0800

    build: set base to relative for production deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0',
     port: 5173,
