9d660586ebe826b19eb061d392933e4f5a528681
tingfeng007 <ganggexiang@gmail.com>
2026-09-24 15:08:07 +0800

    deploy: switch reader to paragraph playback

---
+++ assets/target-NUi6o4OM.js
@@ -0,0 +1,1 @@
+import{T as e}from"./index-CaOm9F5Z.js";var t={name:`target`,size:24,node:[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`circle`,{cx:`12`,cy:`12`,r:`6`,key:`1vlfrh`}],[`circle`,{cx:`12`,cy:`12`,r:`2`,key:`1c9p78`}]]};t.node;var n=e(t);export{n as t};
